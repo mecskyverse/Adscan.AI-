f3099f7e14f4ce4352f3d5967a8274fde30e70f6
Aakash <vermaaakash1000@gmail.com>
2024-03-07 00:03:26 +0530

    Promo action added

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
-import Hero from "@/components/Navigation/Hero/Hero";
+import Hero from "@/components/Hero/Hero";
+import Promo from "@/components/Promo/Promo";
 // import Navbar from "@/components/Navbar";
 export default function Home() {
   return (
     <>
-      <div className="container mx-auto flex justify-center">
+      <div className="container mx-auto flex justify-center flex-col">
        <Hero/>
+       <Promo/>
       </div>
     </>
   );
