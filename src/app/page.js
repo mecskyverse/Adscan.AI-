c48afb78bfd852c1bf1f60f1f99b788301998f60
Aakash <vermaaakash1000@gmail.com>
2024-03-08 18:57:19 +0530

    Components added in main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,26 @@
 import Image from "next/image";
+import { Inter } from "next/font/google";
 import Hero from "@/components/Hero/Hero";
 import Promo from "@/components/Promo/Promo";
+import Brands from "@/components/CompetitorSection/Brands";
+import Research from "@/components/CompetitorSection/Research";
+import SaveAdsFromMeta from "@/components/CompetitorSection/SaveAdsFromMeta";
+import Review from "@/components/Reviews/Review";
+import StartWinning from "@/components/Reviews/StartWinning";
 // import Navbar from "@/components/Navbar";
+
+const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <>
-      <div className="container mx-auto flex justify-center flex-col">
+      <div className="container mx-auto flex justify-center flex-col" >
        <Hero/>
        <Promo/>
+       <Brands/>
+       <Research/>
+       <SaveAdsFromMeta/>
+       <Review/>
+       <StartWinning/>
       </div>
     </>
   );
