b666aaf4f9ddb9fbc42040aba0b0c499c2cc3698
Aakash <vermaaakash1000@gmail.com>
2024-03-10 18:41:47 +0530

    customer review section updated

--- src/components/Reviews/Review.jsx
+++ src/components/Reviews/Review.jsx
@@ -33,7 +33,7 @@ const Review = () => {
                     <div className="space-y-1">
                         <p className={`${inter.className} font-bold text-text-primary css-qaz3k7`}>
                             Dennis Willeboordse</p>
-                        <span className={`${inter.className} MuiTypography-caption1 text-text-secondary css-11mm823`}>
+                        <span className={`${inter.className} text-text-secondary css-11mm823`}>
                             Founder &amp; CEO @D-DOUBLEU MEDIA</span>
                     </div>
                 </div>
@@ -42,27 +42,30 @@ const Review = () => {
                         “Atria helps my team save crazy hours we’d normally spend in other tools. Atria is a powerful all-in-one platform to discover new ads and to craft superior ads in less time - all driven by its unique AI capabilities.”</p>
                 </div>
                 <div className="mt-4 flex justify-center opacity-[0.65]">
-                    {/* <Image alt="Dennis Willeboordse" loading="lazy" width="226" height="52" decoding="async" data-nimg="1" className="w-auto" srcset="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbrand3.5ac609d7.png&amp;w=256&amp;q=75 1x, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbrand3.5ac609d7.png&amp;w=640&amp;q=75 2x" src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbrand3.5ac609d7.png&amp;w=640&amp;q=75" style="color: transparent;"> */}
+                    {/* Here we can add the company logo image */}
+                    {/* <Image alt="Dennis Willeboordse" loading="lazy" width="226" height="52" decoding="async" data-nimg="1" className="w-auto" src="" /> */}
                 </div>
             </div>
             <div className="p-4 sm:p-6 md:p-8 lg:p-10 flex flex-col justify-center items-center [background:radial-gradient(46.25%_46.25%_at_50%_53.75%,rgba(226,82,20,0.40)_0%,rgba(0,0,0,0.40)_100%)]">
                 <div className="flex gap-3 justify-center items-center">
                     <div className="rounded-full overflow-hidden relative w-16 h-16 shadow-[0px_4px_40px_0px_rgba(246,108,48,0.40)]">
-                        {/* <Image alt="Nicole Crowell" loading="lazy" decoding="async" data-nimg="fill" className="object-cover" sizes="100vw" srcset="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fpartner1.776ceeb6.jpeg&amp;w=640&amp;q=75 640w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fpartner1.776ceeb6.jpeg&amp;w=750&amp;q=75 750w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fpartner1.776ceeb6.jpeg&amp;w=828&amp;q=75 828w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fpartner1.776ceeb6.jpeg&amp;w=1080&amp;q=75 1080w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fpartner1.776ceeb6.jpeg&amp;w=1200&amp;q=75 1200w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fpartner1.776ceeb6.jpeg&amp;w=1920&amp;q=75 1920w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fpartner1.776ceeb6.jpeg&amp;w=2048&amp;q=75 2048w, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fpartner1.776ceeb6.jpeg&amp;w=3840&amp;q=75 3840w" src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fpartner1.776ceeb6.jpeg&amp;w=3840&amp;q=75" style="position: absolute; height: 100%; width: 100%; inset: 0px; color: transparent;"> */}
+                        {/* Here we can add the company logo image */}
+                        {/* <Image alt="Dennis Willeboordse" loading="lazy" width="226" height="52" decoding="async" data-nimg="1" className="w-auto" src="" /> */}
                     </div>
                     <div className="space-y-1">
                         <p className={`${inter.className} font-bold text-text-primary css-qaz3k7`}>
                             Nicole Crowell</p>
-                        <span className={`${inter.className} MuiTypography-caption1 text-text-secondary css-11mm823`}>
+                        <span className={`${inter.className} text-text-secondary css-11mm823`}>
                             CEO &amp; Founder @Performance Purple</span>
                     </div>
                 </div>
                 <div className="mt-6">
-                    <p className={`${inter.className} MuiTypography-subheadline font-medium text-text-primary css-18ji250`}>
+                    <p className={`${inter.className}  font-medium text-text-primary css-18ji250`}>
                         “Atria's AI platform, with a vast 5 million ad library, acts like a personal assistant, tailoring searches to my needs. Atria is more than an ad tool; it’s a forward-thinking partner, keeping my strategies ahead of the curve.”</p>
                 </div>
                 <div className="mt-4 flex justify-center opacity-[0.65]">
-                    {/* <Image alt="Nicole Crowell" loading="lazy" width="166" height="52" decoding="async" data-nimg="1" className="w-auto" srcset="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbrand1.9914def9.png&amp;w=256&amp;q=75 1x, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbrand1.9914def9.png&amp;w=384&amp;q=75 2x" src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbrand1.9914def9.png&amp;w=384&amp;q=75" style="color: transparent;"> */}
+                    {/* Here we can add the company logo image */}
+                    {/* <Image alt="Dennis Willeboordse" loading="lazy" width="226" height="52" decoding="async" data-nimg="1" className="w-auto" src="" /> */}
                 </div>
             </div>
         </div>
